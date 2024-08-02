c09df498208eabb6aa212d5706996ed869cb0201
azesm <azesm@yahoo.com>
2024-08-02 23:38:00 +0300

    7 End

--- src/Money.java
+++
@@ -1,5 +0,0 @@
-public class Money {
-    public static void main(String[] args) {
-        System.out.println("Make a money");
-    }
-}
--- src/StudentOrderValidator.java
+++ src/StudentOrderValidator.java
@@ -7,36 +7,40 @@ public class StudentOrderValidator {
         StudentOrder so = readStudentOrder();
 
         AnswerCityRegister cityAnswer = checkCityRegister(so);
-        checkWedding(so);
-        checkChildren(so);
-        checkStudent(so);
+        AnswerWedding wedAnswer = checkWedding(so);
+        AnswerChildren childAnswer = checkChildren(so);
+        AnswerStudent studentAnswer = checkStudent(so);
 
         sendMail(so);
 
     }
 
+    //
     static StudentOrder readStudentOrder() {
         StudentOrder so = new StudentOrder();
         return so;
     }
 
     static AnswerCityRegister checkCityRegister(StudentOrder so) {
-
         System.out.println("CityRegister is running");
         AnswerCityRegister ans = new AnswerCityRegister();
         return ans;
     }
 
-    static void checkWedding(StudentOrder so) {
-        System.out.println("Wedding запущен");
+    static AnswerWedding checkWedding(StudentOrder so) {
+        System.out.println("Wedding is running");
+        return new AnswerWedding();
     }
 
-    static void checkChildren(StudentOrder so) {
+    static AnswerChildren checkChildren(StudentOrder so) {
         System.out.println("Children Check is running");
+        return new AnswerChildren();
     }
 
-    static void checkStudent(StudentOrder so) {
+    static AnswerStudent checkStudent(StudentOrder so) {
+
         System.out.println("Студенты проверяются");
+        return new AnswerStudent();
     }
 
     static void sendMail(StudentOrder so) {
