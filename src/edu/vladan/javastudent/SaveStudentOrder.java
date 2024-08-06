fe1b1ea8f258c5b1f7dfabe64af15dd90f0adeb8
azesm <azesm@yahoo.com>
2024-08-06 16:56:07 +0300

    11End set get

--- src/edu/vladan/javastudent/SaveStudentOrder.java
+++ src/edu/vladan/javastudent/SaveStudentOrder.java
@@ -5,24 +5,18 @@ import edu.vladan.javastudent.domain.StudentOrder;
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.husFirstName = "Alex";
-        so.husLastName = "Petrov";
-        so.wifeFirstName = "Galina";
-        so.wifeLastName = "Petrova";
-
+        so.setHusFirstName("Alex");
+        so.setHusLastName("Petrov");
+        so.setWifeFirstName("Galina");
+        so.setWifeLastName("Petrova");
         long ans = saveStudentOrder(so);
         System.out.println(ans);
 
-        StudentOrder so1 = new StudentOrder();
-        so1.husFirstName = "Bobb";
-        so1.husLastName = "Negrov";
-        so1.wifeFirstName = "Malina";
-        so1.wifeLastName = "Negrova";
     }
 
     static long saveStudentOrder(StudentOrder studentOrder) {
         long answer = 199;
-        System.out.println("saveStudentOrder:" + studentOrder.husLastName);
+        System.out.println("saveStudentOrder:" + studentOrder.getHusLastName());
 
         return answer;
     }
--- src/edu/vladan/javastudent/domain/StudentOrder.java
+++ src/edu/vladan/javastudent/domain/StudentOrder.java
@@ -1,10 +1,41 @@
 package edu.vladan.javastudent.domain;
 
 public class StudentOrder {
-    public String husFirstName;
-    public String husLastName;
-    public String wifeFirstName;
-    public String wifeLastName;
+    private String husFirstName;
+    private String husLastName;
+    private String wifeFirstName;
+    private String wifeLastName;
 
+    public String getHusFirstName() {
+        return husFirstName;
+    }
+
+    public void setHusFirstName(String husFirstName) {
+        this.husFirstName = husFirstName;
+    }
+
+    public String getHusLastName() {
+        return husLastName;
+    }
+
+    public void setHusLastName(String husLastName) {
+        this.husLastName = husLastName;
+    }
+
+    public String getWifeFirstName() {
+        return wifeFirstName;
+    }
+
+    public void setWifeFirstName(String wifeFirstName) {
+        this.wifeFirstName = wifeFirstName;
+    }
+
+    public String getWifeLastName() {
+        return wifeLastName;
+    }
+
+    public void setWifeLastName(String wifeLastName) {
+        this.wifeLastName = wifeLastName;
+    }
 }
 
